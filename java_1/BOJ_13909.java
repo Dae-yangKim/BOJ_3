633de58649f91b42682f7eb0dc9172f8f46d199f
kengo hirano <daeyangkim124@gmail.com>
2024-06-14 23:04:31 +0900

    24.06.14.commit

---
+++ java_1/BOJ_13909.java
@@ -0,0 +1,15 @@
+package java_1;
+
+import java.util.*;
+
+public class BOJ_13909 {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        System.out.println((int) Math.sqrt(n));
+
+        scan.close();
+    }
+}
