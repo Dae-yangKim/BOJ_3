8195a90101e08c328a9773141a565b615415ead0
kengo hirano <daeyangkim124@gmail.com>
2024-06-26 23:57:07 +0900

    add_no_code_file.java

---
+++ java_1/Boj_19941.java
@@ -0,0 +1,24 @@
+package java_1;
+
+import java.util.*;
+import java.io.*;
+
+public class Boj_19941 {
+    public void run() throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 입력
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        // 문자열 입력
+        String s = br.readLine();
+
+        
+    }
+    
+    public static void main(String[] args) throws IOException
+    {}
+}
