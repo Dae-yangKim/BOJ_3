fd7347af8bff4f6b8301b6bcdbea3a451b29b0f5
Dae-yangKim <102850893+Dae-yangKim@users.noreply.github.com>
2024-09-10 00:02:24 +0900

    24.09.10.commit

---
+++ python_3/2747.py
@@ -0,0 +1,15 @@
+from typing import *
+import sys
+
+input = sys.stdin.readline
+
+N : int = int(input())
+
+dp = [0] * (N + 1)
+
+dp[0] = 0 ; dp[1] = 1
+
+for i in range(2 , N + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[-1])
